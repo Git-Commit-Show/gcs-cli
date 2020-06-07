4462f3abbe5cdc007263b3b8dae7a22f17ec850c
Parth Krishna <thenerdsuperuser@gmail.com>
2020-06-07 11:59:42 +0530

    clones awkdown

--- setup.sh
+++ setup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+PWD=`pwd`
+echo $PWD
+
+echo "Installing Libraries..."
+rm -rf /tmp/awk*
+cd /tmp
+git clone https://github.com/tfritzon/awkdown.git
+cd awkdown/
+cp awkdown ~/.local/bin/
+clear
+
+cd ${PWD}
+
+pwd
+
 echo "Making file executable..."
 chmod +x ./gcs.sh;
 sleep 2;
