d70e04be7297f150ba9fdcf9bc5a000a6a04e3f5
adityayadav800 <cooladityaamo@gmail.com>
2020-06-08 16:03:56 +0530

    Added Check script to clone source

---
+++ check.sh
@@ -0,0 +1,10 @@
+username=$(git config user.name)
+url=$(git config --get remote.origin.url)
+IFS='/' read -r -a array <<< "$url"
+if [ "$username"="${array[3]}" ];
+then
+echo " The repo is cloned from the forked repo";
+else
+echo " The Repo is cloned from ${url}"
+fi
+
