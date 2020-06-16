4f3b2d2ee7972b948b342060293ea1ed342757a4
Parth Krishna <thenerdsuperuser@gmail.com>
2020-06-16 16:33:10 +0530

    Added codes

--- api.sh
+++ api.sh
@@ -8,38 +8,40 @@ TREE="$URL/git/trees"
 BLOB="$URL/git/blobs"
 
 echo "Let us get the latest commit..."
-# curl -sS $URL/commits
 
-
-# NEED TO FIND LATEST COMMIT SHA
-SHA="d2910fb67a0fbb2ba310cb2e240564612bbec958"
-
+# STORE LATEST COMMIT HASH
+SHA=$(curl -sO /dev/null $URL/commits | ./jq-linux64 -r '.[0].sha')
 
 # GET DIR LISTING
-DIR=$TREE/$SHA
+DIR=$TREE/${SHA}
 
 
 # FIND PATH=2020, and it's URL
-PATH_SHA="4661d8397ffbd0bd37aad28d9bc95ccb478b4a7a"
-FILES=$TREE/PATH_SHA
+PATH_SHA=$(curl -sO /dev/null $DIR | ./jq-linux64 -r '.tree[] | select(.path == "2019") | .sha')
 
+FILES=$TREE/$PATH_SHA
+echo $FILES
 
+asd=$(curl -sO /dev/null $FILES | ./jq-linux64 '.tree | .[].path')
+
+echo $asd
 # ITERATE THRU EACH FILE, GET BLOB URL AND NAME
-NAME="parth.md"
-BLOB_SHA="348e8d35ff261bf368ca04d765638e42180e945f"
-BLOB_URL=$BLOB/$BLOB_SHA
+
+#BLOB_SHA="348e8d35ff261bf368ca04d765638e42180e945f"
+#BLOB_URL=$BLOB/$BLOB_SHA
 
 # GET content FROM EACH BLOB
 
-CONTENT="IyMgRGV0YWlscwoKMS4gTmFtZTogUGFydGggS3Jpc2huYSAgIAoyLiBUd2l0\ndGVyOiBbdmltb3ZlcmVtYWNzXShodHRwczovL3d3dy50d2l0dGVyLmNvbS92\naW1vdmVyZW1hY3MpICAgCjMuIExpbmtlZEluOiBbUGFydGggS3Jpc2huYV0o\naHR0cHM6Ly93d3cubGlua2VkaW4uY29tL2luL3BhcnRoa3Jpc2huYSkgICAK\n"
+#CONTENT="IyMgRGV0YWlscwoKMS4gTmFtZTogUGFydGggS3Jpc2huYSAgIAoyLiBUd2l0\ndGVyOiBbdmltb3ZlcmVtYWNzXShodHRwczovL3d3dy50d2l0dGVyLmNvbS92\naW1vdmVyZW1hY3MpICAgCjMuIExpbmtlZEluOiBbUGFydGggS3Jpc2huYV0o\naHR0cHM6Ly93d3cubGlua2VkaW4uY29tL2luL3BhcnRoa3Jpc2huYSkgICAK\n"
 
 # SED TO REPLPACE \n
-OUTPUT="${CONTENT//\\n/}"
+#OUTPUT="${CONTENT//\\n/}"
 
 # RESULT=`echo -n $OUTPUT | base64 --decode`
-echo -n $OUTPUT | base64 --decode > $FILE_PATH/$NAME
+#echo -n $OUTPUT | base64 --decode > $FILE_PATH/$NAME
 
 #echo $RESULT
-cat $NAME
+#cat $NAME
+
 
 
---
+++ md/parth.md
@@ -0,0 +1,5 @@
+## Details
+
+1. Name: Parth Krishna   
+2. Twitter: [vimoveremacs](https://www.twitter.com/vimoveremacs)   
+3. LinkedIn: [Parth Krishna](https://www.linkedin.com/in/parthkrishna)   
